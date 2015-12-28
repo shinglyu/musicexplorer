34ef73fc45764eab669c6895f9328a97f97cdfb5
Shing Lyu <shing.lyu@gmail.com>
2015-12-28 13:32:52 +0800

    Added chinese

---
+++ corpus-chinese.js
@@ -0,0 +1,12 @@
+var tags = [
+  {
+    'name':'instruments',
+    'tags':[
+//Retrived from http://musiced.about.com/od/musicinstruments/a/instruments.htm 2015/3/28
+      '古琴',
+      '崑曲',
+      '越劇',
+    ]
+  }
+]
+
